cae662258cffe7a9f2ae1ed299b139aec9245a0e
Elena Egorshina <Egorshinae@yandex.ru>
2023-02-08 20:59:04 +0400

    Сorrected by Сamelcase

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,13 @@ public class Main {
 
         int amount = 15_000; // стоимость 1 билета в руб
 
-        int need_for_mile = 20; // рублей для 1 бонусной мили
+        int needForMile = 20; // рублей для 1 бонусной мили
 
-        int mile = amount / need_for_mile;
+        int mile = amount / needForMile;
 
         System.out.println("Итого миль: " + mile);
 
     }
 
-    
+
 }
